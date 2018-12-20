55acdeca669d07bde22bebcbdd21a5d27532facd
Aaron <39026927+xaaron3@users.noreply.github.com>
2018-12-20 14:38:06 -0500

    hooks added, old class based method in // notes

--- src/index.js
+++ src/index.js
@@ -1,33 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
 
-class App extends React.Component {
-  state = { lat: null, errorMessage: '' };
+const App = () => {
+  // first ele of array is value of state prop, 2nd is a function to change it. 
+  const [lat, setLat] = useState(null);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  componentDidMount() {
+  useEffect(() => {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
+      position => setLat(position.coords.latitude),
+      err => setErrorMessage(err.message)
     );
+  }, []);
+  // empty array means only run once during entire lifecycle of component
+
+
+  let content;
+  if (errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if (lat) {
+    content = <SeasonDisplay lat={lat} />
+  } else {
+    content = <Spinner message="Please accept location request" />
   }
 
-  renderContent() {
-    if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
-    }
 
-    if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
-    }
-
-    return <Spinner message="Please accept location request" />;
-  }
-
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
+  return <div className="border red">{content}</div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
+
+// old class method
+// class App extends React.Component {
+//   state = { lat: null, errorMessage: '' };
+
+//   componentDidMount() {
+//     window.navigator.geolocation.getCurrentPosition(
+//       position => this.setState({ lat: position.coords.latitude }),
+//       err => this.setState({ errorMessage: err.message })
+//     );
+//   }
+
+//   renderContent() {
+//     if (this.state.errorMessage && !this.state.lat) {
+//       return <div>Error: {this.state.errorMessage}</div>;
+//     }
+
+//     if (!this.state.errorMessage && this.state.lat) {
+//       return <SeasonDisplay lat={this.state.lat} />;
+//     }
+
+//     return <Spinner message="Please accept location request" />;
+//   }
+
+//   render() {
+//     return <div className="border red">{this.renderContent()}</div>;
+//   }
+// }
